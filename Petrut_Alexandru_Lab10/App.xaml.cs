8724f43d479fdacb960ec3331437c61b06520422
Petrut Alexandru-Bogdan <alexpetrut12@yahoo.com>
2021-12-10 00:18:47 +0200

    Add project files.

---
+++ Petrut_Alexandru_Lab10/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using Petrut_Alexandru_Lab10.Data;
+using System.IO;
+
+namespace Petrut_Alexandru_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
